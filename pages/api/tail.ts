c77177faf5adc13cfdd0edcfd0f84d3f1814cd91
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2021-09-14 02:57:10 +0300

    kek

--- pages/api/tail.ts
+++ pages/api/tail.ts
@@ -1,13 +1,84 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { gql } from '@apollo/client';
+import { apolloClient } from '../../apollo-client';
+import path from 'path';
+import { readFile } from 'fs/promises';
 
-interface Data {
-  json_id: string
+interface RequestData {
+  input: {
+    tail: string;
+  }
 }
 
-export default function handler(
+interface ErrorResponseData {
+  message: string;
+}
+
+export interface ResponseData extends Omit<TailData, 'id'>{
+  json_id: number;
+}
+
+interface JsonIdsResponseData {
+  long_tails: {
+    json_id: number;
+  }[];
+}
+
+interface TailData {
+  id: number;
+  title: string;
+  description: string;
+}
+
+const queryJsonIds = gql`
+query ($tail: String!) {
+  long_tails(where: {tail: {_eq: $tail}}) {
+    json_id
+  }
+}
+`;
+
+const getTailData = async (json_id: number): Promise<TailData | undefined> => {
+  const json = await readFile(path.join(process.cwd(), 'tails.json'), 'utf-8');
+  const titles = JSON.parse(json) as TailData[];
+
+  return titles.find(({ id }) => id === json_id);
+}
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ResponseData | ErrorResponseData>
 ) {
-  res.status(200).json({ json_id: 'John Doe' })
+  const { tail } = (req.body as RequestData).input;
+
+  const { long_tails: tails } = (await apolloClient.query<JsonIdsResponseData>({
+    query: queryJsonIds,
+    variables: {
+      tail
+    }
+  })).data;
+
+  if (!tails.length) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+
+  const { json_id } = tails[0];
+
+  let tailData: TailData;
+  try {
+    tailData = await getTailData(json_id);
+  } catch (err: unknown) {
+    return res.status(400).json({ message: err instanceof Error ? err.message : 'Internal server error' });
+  }
+
+  const { title, description } = tailData;
+
+  console.log(title);
+  
+  res.status(200).json({
+    json_id,
+    title,
+    description,
+  })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,31 +4,55 @@ import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import { gql } from '@apollo/client';
 import { apolloClient } from '../apollo-client';
+import { ResponseData } from './api/tail';
 
-const Home: NextPage = () => {
+const fetchTail = async (tail: string) => {
+  const { data } = await apolloClient.query<ResponseData>({
+    query: gql`
+      query ($tail: String!) {
+        getTailId(tail: $tail) {
+          json_id
+          title
+          description
+        }
+      }
+    `,
+    variables: {
+      tail
+    }
+  });
+  return data;
+}
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+const Home: NextPage<Props> = (props) => {
+  const { title, description } = props;
   return (
-    <div>
-      123
-    </div>
+    <main>
+      <div>
+        title: {title}
+      </div>
+      <div>
+        description: {description}
+      </div>
+    </main>
   )
 }
 
 export default Home
 
-export const getServerSideProps = async () => {
-  const { data } = await apolloClient.query({
-    query: gql`
-      query MyQuery {
-        getTailId(tail: "best-hello-ever") {
-          json_id
-        }
-      }
-    `
-  });
-
+export const getServerSideProps = async (context: any): Promise<{ props: Props }> => {
+  const { tail } = context.params;
+  const { title, description } = await fetchTail(tail)
+  
   return {
     props: {
-
+      title,
+      description
     }
   }
 }

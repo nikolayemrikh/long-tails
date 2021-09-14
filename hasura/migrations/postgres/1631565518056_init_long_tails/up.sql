71a4e38f5364179672e70c21d55931f50c172980
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2021-09-14 23:35:35 +0300

    readme

---
+++ hasura/migrations/postgres/1631565518056_init_long_tails/up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE long_tails (
+  id serial NOT NULL,
+  tail text NOT NULL unique,
+  json_id integer NOT NULL unique,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO long_tails (tail, json_id) VALUES ('best-hello-ever', 1);
+INSERT INTO long_tails (tail, json_id) VALUES ('best-hello-world-ever', 2);
+INSERT INTO long_tails (tail, json_id) VALUES ('best-world-ever', 3);

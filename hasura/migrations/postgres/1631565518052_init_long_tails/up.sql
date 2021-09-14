c5a6a4b767340527ce9e9c61a5215b7336899b40
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2021-09-14 23:13:06 +0300

    chore: add data migration

--- hasura/migrations/postgres/1631565518052_init_long_tails/up.sql
+++ hasura/migrations/postgres/1631565518052_init_long_tails/up.sql
@@ -4,3 +4,7 @@ CREATE TABLE long_tails (
   json_id integer NOT NULL unique,
   PRIMARY KEY (id)
 );
+
+INSERT INTO long_tails (tail, json_id) VALUES ("best-hello-ever", 1);
+INSERT INTO long_tails (tail, json_id) VALUES ("best-hello-world-ever", 2);
+INSERT INTO long_tails (tail, json_id) VALUES ("best-world-ever", 3);

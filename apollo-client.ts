bef33c4e6e51434b8f76d90b165781614317bdf1
Nikolay Emrikh <nikolay.emrikh@fundraiseup.com>
2021-09-15 12:08:26 +0300

    fix: docker duild

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,6 +1,6 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
-  uri: `http://${process.env.NODE_ENV === 'production' ? 'host.docker.internal' : 'localhost:8080'}/v1/graphql`,
+  uri: `http://${process.env.NODE_ENV === 'production' ? 'host.docker.internal:8080' : 'localhost:8080'}/v1/graphql`,
   cache: new InMemoryCache(),
 });
